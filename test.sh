5c10414483c0d5325dca5676128353881fd49529
Jonathan Gould <jonathan@donorpage.io>
2020-01-08 10:42:31 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,10 +1,14 @@
-echo "lls  /Users/runner/Library"
+echo "ls  /Users/runner/Library"
 echo "---------------------------------------------------"
 ls  /Users/runner/Library
 
-echo "lls  /Users/runner/Library/Android"
+echo "ls  /Users/runner/Library/Android"
 echo "---------------------------------------------------"
 ls  /Users/runner/Library/Android
+
+echo "ls  /Users/runner/Library/Android/sdk"
+echo "---------------------------------------------------"
+ls  /Users/runner/Library/Android/sdk
 
 echo "ls /Users/runner/Library/Android/sdk/ndk-bundle"
 echo "-----------------------------------------------"
